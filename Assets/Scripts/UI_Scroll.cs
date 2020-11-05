bbf5d7e43dda7f9ed975371d08cc283fc645df1b
1pwny <sammy27berger@gmail.com>
2020-11-05 16:09:16 -0500

    First commit

---
+++ Assets/Scripts/UI_Scroll.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_Scroll : Button
+{
+    public GameObject me;
+    public Image closed, opened;
+
+    private bool open = false; //true = open scroll, false = closed scroll
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(open)
+        {
+            opened.gameObject.SetActive(true);
+            closed.gameObject.SetActive(false);
+        }
+        else
+        {
+            opened.gameObject.SetActive(false);
+            closed.gameObject.SetActive(true);
+        }
+    }
+
+    //furls or unfurls the scroll
+    public void furl()
+    {
+        open = !open;
+        Debug.Log("furled? " + open);
+    }
+
+}
